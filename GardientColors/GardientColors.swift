9571fe0fad6b50dbf3e6b4d8b3c8338787a1b3e6
iSimoo <isimoo.apple@gmail.com>
2021-08-02 11:28:54 +0400

    Initial Commit

---
+++ GardientColors/GardientColors.swift
@@ -0,0 +1,36 @@
+//  Creating GradientColor inside Stroyboard
+
+//  To add gradient color to Labels just change the class declaration from UIView to UILabel
+
+//  You can add more colors to the layer.colors array as much as you can
+
+//  Created by iSimoo on 8/2/21 . . . Happy Coding.
+
+import UIKit
+
+@IBDesignable
+
+class GradientColors : UIView {
+    
+    @IBInspectable var FirstColor : UIColor = .clear {
+        didSet { updateView() }
+    }
+    
+    @IBInspectable var SecondColor : UIColor = .clear {
+        didSet { updateView() }
+    }
+    
+    
+    override class var layerClass: AnyClass {
+        get{ return CAGradientLayer.self }
+    }
+    
+    func updateView() {
+        let layer = self.layer as! CAGradientLayer
+        layer.colors = [FirstColor.cgColor, SecondColor.cgColor]
+        //        layer.locations = [0.5]
+    }
+    
+    
+    
+}
